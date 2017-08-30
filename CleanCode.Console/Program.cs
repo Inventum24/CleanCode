439c8b983b18f1dcd96679ced180095228c6c0bb
IMPAQ\pnow <piotr.nowak@cumulocity.com>
2017-08-30 16:07:02 +0200

    SOLID

--- CleanCode.Console/Program.cs
+++ CleanCode.Console/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CleanCode.Console.ProgramistaByc.SOLID._03LSP;
+using CleanCode.Console.ProgramistaByc.SOLID._04ISP;
+using CleanCode.Console.CSharpHandbook._03Usage;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +13,40 @@ namespace CleanCode.Console
     {
         static void Main(string[] args)
         {
+            //LSP();
+            ISP();
+
+            Enums.TestEnums();
+
+            System.Console.ReadKey();
+        }
+
+        private static void ISP()
+        {
+            var electricCar = new ElectricCarIF();
+            var car = new CarIF();
+
+            car.Name = "Tesla";
+            car.WheelsCount = 4;
+            car.Type = "Muscle Car";
+            car.Combustion = 10.5f;
+
+            electricCar.Name = "Tesla";
+            electricCar.WheelsCount = 4;
+            electricCar.BatteriesCount = 10;
+
+            electricCar.About();
+            car.About();
+
+            IVehicle ec = (IVehicle)electricCar;
+
+
+        }
+
+        private static void LSP()
+        {
+            ElectricCar elCar = new ElectricCar(10, -100, "The Best Electric Car", 4);
+            elCar.ShowInfoAboutVehicle(elCar);
         }
     }
 }
